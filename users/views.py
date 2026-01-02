c0aa48234e68a4f892059126f91777aca373d21b
Sasha Gershtein <sasha@gershtein.net>
2026-01-02 23:54:17 +0000

    Add home view and base templates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
 
-def home(request):
-    return render(request, 'users/unauthorised.html')
+class Home(View):
+    def get(self, request):
+        return render(request, 'users/unauthorised.html')
