7e16a99f910aca71e0522ca3a1dc47656015881e
Sasha Gershtein <sasha@gershtein.net>
2026-01-03 00:21:03 +0000

    Add static

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('auth', views.Auth.as_view(), name='auth'),  # temp debug view
 ]
--- users/views.py
+++ users/views.py
@@ -5,3 +5,7 @@ from django.views import View
 class Home(View):
     def get(self, request):
         return render(request, 'users/unauthorised.html')
+
+class Auth(View):  # temp debug view
+    def get(self, request):
+        return render(request, 'users/auth_base.html')
