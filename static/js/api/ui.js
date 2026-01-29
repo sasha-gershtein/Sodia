216b17ecd7f2c92bbbff99460da04b2806452806
Sasha Gershtein <sasha@gershtein.net>
2026-01-29 12:28:16 +0000

    Add new message display api

--- static/js/api/ui.js
+++ static/js/api/ui.js
@@ -1,15 +1,43 @@
-const error_box = document.getElementById("error-box");
-let hide_error_box = 0;
+// noinspection JSUnusedGlobalSymbols
+
+const info_box = document.getElementById("info-box");
+
+export class InfoMessage {
+    constructor(message, level, timeout = 10000) {
+        this.message = message;
+        if (level !== "info" && level !== "warning" && level !== "error" && level !== "success") {
+            throw TypeError(`level should be "info", "warning", "error", or "success", not "${level}"`)
+        }
+        this.level = level;
+        this.element = document.createElement("div");
+        this.element.classList.add("info-message");
+        this.element.classList.add(this.level);
+        this.element.innerHTML = this.message;
+        info_box.appendChild(this.element);
+        this.displayed = true;
+        this.element.addEventListener("click", this.remove.bind(this));
+        setTimeout(this.remove.bind(this), timeout);
+    }
+
+    remove() {
+        if (!this.displayed) return;
+        info_box.removeChild(this.element);
+        this.displayed = false;
+    }
+}
+
+export function displayInfo(message) {
+    return new InfoMessage(message, "info")
+}
+
+export function displayWarning(message) {
+    return new InfoMessage(message, "warning")
+}
 
 export function displayError(message) {
-    if (!error_box) {
-        alert(`error: ${message}`);
-        return;
-    }
-    error_box.innerText = message;
-    error_box.classList.remove("hidden");
-    hide_error_box = new Date().getTime() + 4900;
-    setTimeout(function () {
-        if (hide_error_box < new Date().getTime()) error_box.classList.add("hidden");
-    }, 5000)
+    return new InfoMessage(message, "error")
+}
+
+export function displaySuccess(message) {
+    return new InfoMessage(message, "success")
 }
