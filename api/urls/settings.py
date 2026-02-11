ff1a84d3f30eb1c7a1e2b80aa175f642c9ebd9d1
Sasha Gershtein <sasha@gershtein.net>
2026-02-11 19:59:54 +0000

    Fix trailing slashes in urls

--- api/urls/settings.py
+++ api/urls/settings.py
@@ -5,8 +5,8 @@ from settings import api
 app_name = 'settings'
 
 urlpatterns = [
-    path('account', api.account, name='account'),
-    path('privacy', api.privacy, name='privacy'),
-    path('notifications', api.notifications, name='notifications'),
-    path('challenges', api.challenges, name='challenges'),
+    path('account/', api.account, name='account'),
+    path('privacy/', api.privacy, name='privacy'),
+    path('notifications/', api.notifications, name='notifications'),
+    path('challenges/', api.challenges, name='challenges'),
 ]
--- api/urls/users.py
+++ api/urls/users.py
@@ -5,8 +5,8 @@ from users import api
 app_name = 'users'
 
 urlpatterns = [
-    path('login', api.login, name='login'),
-    path('logout', api.logout, name='logout'),
-    path('register', api.register, name='register'),
-    path('change-password', api.change_password, name='change_password'),
+    path('login/', api.login, name='login'),
+    path('logout/', api.logout, name='logout'),
+    path('register/', api.register, name='register'),
+    path('change-password/', api.change_password, name='change_password'),
 ]
